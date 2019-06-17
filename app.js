48e7761ac660ac0def3a5af80a6b74f45d687467
raj <raj.rana507@gmail.com>
2019-06-17 15:43:59 +0530

    fial category list generated

--- app.js
+++ app.js
@@ -6,28 +6,42 @@ var fs = require('fs');
 var qaurkcmSelectedCategories = require('./assets/tomtop-categories.json'); // this is are the selected categories that should be there
 var qaurkcmCategories = require('./assets/quarkscm-categories.json'); 
 var newtest = require('./assets/new-test.json'); // this is are quarkscm categories
-
+var qaurkcmMappedCategories = require('./assets/quarkscm-mapped-categories.json');
+var quarkscmthirdlevel = require('./assets/third-level-empty-array-removed.json');
 var array = qaurkcmCategories.data;
 
 
+// this is removing all the level 1 categories with children 0
+quarkscmthirdlevel = quarkscmthirdlevel.filter((obj)=>{
+    return obj.children.length > 0;
+}) 
 
+//  this loop was for removing the ojects which children were empty at level 2
+// for (let i = 0; i < qaurkcmMappedCategories.length; i++) {
+//     for (let j = 0; j < qaurkcmMappedCategories[i].children.length; j++) {
+//         qaurkcmMappedCategories[i].children = qaurkcmMappedCategories[i].children.filter((obj)=>{
+//             return obj.children.length > 0 ;
+//         })
+//     }
+
+// }
 
 // this loop is for  removing the childerns which we dont want in my case all the removing of the children 
 // was done at the level 3 that is why i have dobne in two forloops
-for (let i = 0; i < newtest.length; i++) {
-    for (let j = 0; j < newtest[i].children.length; j++) {       
-        newtest[i].children[j].children = newtest[i].children[j].children.filter((obj1)=>{
-            return qaurkcmSelectedCategories.some((obj2)=>{
-                return obj1._id == obj2.Quark_id;
-            }); 
-        });        
-    }
-}
+// for (let i = 0; i < newtest.length; i++) {
+//     for (let j = 0; j < newtest[i].children.length; j++) {       
+//         newtest[i].children[j].children = newtest[i].children[j].children.filter((obj1)=>{
+//             return qaurkcmSelectedCategories.some((obj2)=>{
+//                 return obj1._id == obj2.Quark_id;
+//             }); 
+//         });        
+//     }
+// }
 
-var data = newtest;
+var data = quarkscmthirdlevel;
 
 //  for  writing any files 
-fs.writeFile("./assets/new-filter.json", JSON.stringify(data), (err) => {
+fs.writeFile("./assets/final-category-tree.json", JSON.stringify(data), (err) => {
     if (err) console.log(err);
     console.log("Successfully Written to File.");
 });
